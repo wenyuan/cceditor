53314f716b6faae136571be0d2219b2d9d13c722
wenyuan <xuwenyuan2016@163.com>
2019-09-19 11:23:32 +0800

    配置: 修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
 }
 
 module.exports = {
-  // 修改 src 为 examples
+  // 修改src为examples
   pages: {
     index: {
       entry: 'examples/main.js',
@@ -18,16 +18,16 @@ module.exports = {
     config.resolve.alias
       .set('@', resolve('examples')); // key,value自行定义，比如.set('@@', resolve('src/components'))
     // 扩展 webpack 配置，使 packages 加入编译
-    // config.module
-    //   .rule('js')
-    //   .include
-    //   .add('packages')
-    //   .end()
-    //   .use('babel')
-    //   .loader('babel-loader')
-    //   .tap(options => {
-    //     // 修改它的选项...
-    //     return options
-    //   })
+    config.module
+      .rule('js')
+      .include
+      .add(path.resolve(__dirname, 'packages'))
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options;
+      });
   }
 };
